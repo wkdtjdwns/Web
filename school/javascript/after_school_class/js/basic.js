1b82599fa292b2ceed622bead1a8228645217558
Sung Jun Jang <128266768+wkdtjdwns@users.noreply.github.com>
2023-12-14 18:38:47 +0900

    Create basic.js

---
+++ school/javascript/after_school_class/js/basic.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <script>
+        // 자바스크립트도 인라인 방식으로 적용할 수 있음 (<script> 태그)
+        console.log('Hello World'); // 개발자들만 볼 수 있도록 메모할 수 있음 (F12를 누른 뒤 Console 탭에서 확인 가능) -> 웹 페이지에는 보이지 않음
+        document.write('Hello World'); // 모두가 볼 수 있도록 메모할 수 있음 -> 웹 페이지에서 볼 수 있음
+
+        // console.log를 사용하는 쪽이 변수 같은 것들을 효과적으로 확인할 수 있어서 보다 많이 사용됨
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
